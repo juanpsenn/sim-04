308c224033f5d36ab710d362a5c1529ea3f6a8f9
juanpsenn <juanpsenn@gmail.com>
2020-05-14 22:22:39 -0300

    Updates

---
+++ precache-manifest.cce886eaf211f6564042afd6b7805404.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd2c43420ee89475bc2e5d62d5acb7f",
+    "url": "/sim-04/index.html"
+  },
+  {
+    "revision": "ba138a241668d620794e",
+    "url": "/sim-04/static/css/2.214ad93a.chunk.css"
+  },
+  {
+    "revision": "deca5731e2f8c73afa62",
+    "url": "/sim-04/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ba138a241668d620794e",
+    "url": "/sim-04/static/js/2.99a29e6c.chunk.js"
+  },
+  {
+    "revision": "664edcf1e03a7581cf8234ad9df15e41",
+    "url": "/sim-04/static/js/2.99a29e6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deca5731e2f8c73afa62",
+    "url": "/sim-04/static/js/main.3a9b1f8a.chunk.js"
+  },
+  {
+    "revision": "49f55ca28e1e1d0c8637",
+    "url": "/sim-04/static/js/runtime-main.10ce4342.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sim-04/precache-manifest.cce886eaf211f6564042afd6b7805404.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sim-04/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
